533540d0b27d9a3dae7cf3d73afe574e9d691199
grimmal72 <jordanmalmgren@gmail.com>
2026-07-20 19:38:37 -0700

    Add files via upload

---
+++ ( examples of things/SDL/Window/Simplest example of creating an SDL window that stays up.cpp
@@ -0,0 +1,53 @@
+#include <SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *args[])
+{
+	// The window we'll be rendering to
+	SDL_Window *window = NULL;
+
+	// The surface contained by the window
+	SDL_Surface *screenSurface = NULL;
+
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		// Create window
+		window = SDL_CreateWindow("SDL Window Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			// Kill program by not entering the while loop
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			// Keep window up
+			SDL_Event event;
+			bool quit = false;
+
+			while (quit == false)
+			{
+				while (SDL_PollEvent(&event))
+				{
+					if (event.type == SDL_QUIT)
+						quit = true;
+				}
+			}
+		}
+	}
+
+	// Destroy window
+	SDL_DestroyWindow(window);
+
+	// Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
+}
